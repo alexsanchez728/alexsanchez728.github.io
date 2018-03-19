60c210b4a93f2d87a427ae46c0c7555c8102fe93
Alex Sanchez <alesanchez728@gmail.com>
2018-03-19 16:30:59 -0500

    started blog rework

--- javascripts/controllers/BlogCtrl.js
+++ javascripts/controllers/BlogCtrl.js
@@ -7,22 +7,29 @@ app.controller("BlogCtrl", function( $scope, DatabaseServices) {
   $scope.showBlog = false;
 
   const checkOutTheseBlogs = () => {
-  
+    
     DatabaseServices.getContent("blogs").then((_blogs) => {
-  
+      
+      // trying to replace any double spaces with a [return]
+      // _blogs.forEach(blog => {
+        
+      //   var thisBlog = blog;
+      //   thisBlog.content.replace(/(\s{2})/g, '+');
+      //   console.log(thisBlog.content);
+    
+      // });
+
       $scope.blogs = _blogs;
 
     }).catch((err) => {
       console.log("error in blogsOrProjects", err);
-    });
-  };
+    });  
+  };  
   checkOutTheseBlogs();
   
   $scope.moreInfo = ((blog) => {
     $scope.chosenBlog = angular.copy(blog);
-    $scope.showBlog = true;
-    // ("#blogJumbotron").html(blog);
-    
+    $scope.showBlog = true;    
   });
 
-}); // end controller
+});
